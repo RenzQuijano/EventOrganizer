8fc50455b244cec69872c302859624e73f0bcb89
RenzQuijano <renzmichaelquijano@gmail.com>
2025-03-15 10:48:14 -0400

    EventOrganizer.java Update
    - Added method to convert input String into DateTime object.
    
    -Added try block to handle user inputs
    
    -Made it so user has to enter a number for number of invitees

--- src/main/EventOrganizer.java
+++ src/main/EventOrganizer.java
@@ -1,7 +1,10 @@
 package main;
 import util.DateTime;
+import util.Date;
+import util.DateTimeInvalidException;
 import util.Event;
 
+import java.io.IOException;
 import java.util.*;
 
 
@@ -9,7 +12,7 @@ public class EventOrganizer {
 
     private static Scanner keyboard = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         ArrayList<Event> list = new ArrayList<>();
 
@@ -21,25 +24,67 @@ public class EventOrganizer {
             if (givenEvent.equalsIgnoreCase("done")) {
                 break;
             }
-            String [] eventProperties = givenEvent.split( ",");
+            String [] eventProperties = givenEvent.split( ", ");
             if (eventProperties.length != 6) {
                 System.out.println("Illegal event entry. Please try again");
                 continue;
             }
 
+            //EX: Name, 4/18/2004 @ 12:00:00 am, 4/18/2004 @ 12:00:00 pm, Host, 5, Location
             String eventName = eventProperties[0];
             String startDateTimeString = eventProperties[1];
             String endDateTimeString = eventProperties[2];
             String hostName = eventProperties[3];
-            String numOfInviteesString = eventProperties[4];
+            try {
+                Integer.parseInt(eventProperties[4]);
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a number for amount of invitees. Ex: 56, 23, 91");
+                continue;
+            }
+            int numOfInvitees = Integer.parseInt(eventProperties[4]);
+
+
             String location = eventProperties[5];
 
             //split up date and time
+            //splits into [MM/DD/YYYY] [@] [hh:mm:ss] [am or pm]
+            String [] startDateTimeProperties = startDateTimeString.split(" "); //splits into [MM/DD/YYYY] [@] [hh:mm:ss] [am or pm]
+            String [] startDateProperties = startDateTimeProperties[0].split("/"); //splits into [MM] [DD] [YYYY]
+            String [] startTimeProperties = startDateTimeProperties[2].split(":"); //splits into [hh] [mm] [ss]
+
+            String [] endDateTimeProperties = endDateTimeString.split(" "); //splits into [MM/DD/YYYY] [@] [hh:mm:ss] [am or pm]
+            String [] endDateProperties = endDateTimeProperties[0].split("/"); //splits into [MM] [DD] [YYYY]
+            String [] endTimeProperties = endDateTimeProperties[2].split(":"); //splits into [hh] [mm] [ss]
+
+            try {
+                DateTime startDateTime = getDateTime(startDateProperties, startTimeProperties, startDateTimeProperties);
+                DateTime endDateTime = getDateTime(endDateProperties, endTimeProperties, endDateTimeProperties);
+
+                if(!DateTime.isValidDateTime(startDateTime)) {
+                    throw new DateTimeInvalidException(startDateTime); //catch exception!!!
+
+                } else if (!DateTime.isValidDateTime(endDateTime)) {
+                    throw new DateTimeInvalidException(endDateTime);
+                }
+
+                list.add(new Event(eventName, startDateTime, endDateTime, hostName, numOfInvitees, location));
+
+
+            }
+            catch (DateTimeInvalidException e) {
+                System.out.println("Error occurred: " + e);
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Error occurred. Please make sure to write date/time's correctly");
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Error: Please make sure to format date/time's correctly");
+            }
 
 
 
-            Event event = new Event();
-            list.add(event);
+
+            //list.add();
 
         }
         Collections.sort(list);
@@ -76,5 +121,21 @@ public class EventOrganizer {
                 System.out.println("Illegal command. Please try again");
             }
         }
+    }
+
+    private static DateTime getDateTime(String[] dateProperties, String[] timeProperties, String[] dateTimeProperties) {
+        int month = Integer.parseInt(dateProperties[0]),
+                day = Integer.parseInt(dateProperties[1]),
+                year = Integer.parseInt(dateProperties[2]);
+
+        int hour = Integer.parseInt(timeProperties[0]),
+                minute = Integer.parseInt(timeProperties[1]),
+                second = Integer.parseInt(timeProperties[2]);
+
+        if(!dateTimeProperties[3].equalsIgnoreCase("am") || !dateTimeProperties[3].equalsIgnoreCase("pm")) {
+            return new DateTime(new Date(day, month, year), hour, minute, second, dateTimeProperties[4].equalsIgnoreCase("am"));
+        }
+
+        return new DateTime(new Date(day, month, year), hour, minute, second, dateTimeProperties[3].equalsIgnoreCase("am"));
     }
 }
